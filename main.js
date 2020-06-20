dec6ca76b8d4ef6def36e8ce1ee845a9619f4955
play.js <git@gituser.com>
2020-06-19 21:58:09 -0400

    edit front-end and add back-end file

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+//require back-end
+const time = require('/.timelygreeting.js');
+const taken = require('/.tometrc.js');
+const calculator = require('/.caffeinecalculator.js')
+const advice = require('/.advise.js')
+
+let message = '';//Starting string
+let amount = 0;//Starting amount 0 mg
+
+
 
 //getInput
 function getInput(i) {
